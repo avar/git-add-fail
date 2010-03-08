cda6e02683cabc994defe845af6e973909ee32f3
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-03-08 14:45:24 +0000

    Don't print so much crap

--- bisect.sh
+++ bisect.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-help = <<EOF
- git bisect start
- git bisect bad
- git bisect good v1.6.3
- git bisect run ~/g/git-add-fail/bisect.sh
-EOF
+# help = <<EOF
+#  git bisect start
+#  git bisect bad
+#  git bisect good v1.6.0
+#  git bisect run ~/g/git-add-fail/bisect.sh
+# EOF
 
-git clean -dxf
+git clean -dxf > /dev/null
 
-make prefix=/usr all
+make prefix=/usr all > /dev/null 2>&1
 
-GIT=$PWD/git prove ~/g/git-add-fail/add-fail.t
+GIT=$PWD/git prove ~/g/git-add-fail/add-fail.t | grep ^Result
 ret=$?
 
-git clean -dxf
+git clean -dxf > /dev/null
 
 exit $ret
